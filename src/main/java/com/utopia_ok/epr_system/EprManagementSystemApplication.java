6fec2957a38c3dfe43c3e465a797b448b473619e
ivanJoshua98 <ivan.sanmartin98@gmail.com>
2026-01-19 12:20:20 -0300

    Spring Boot project files were loaded

---
+++ src/main/java/com/utopia_ok/epr_system/EprManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.utopia_ok.epr_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EprManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EprManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utopia_ok/epr_system/EprManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utopia_ok.epr_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EprManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
